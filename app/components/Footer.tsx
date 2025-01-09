d2ca8133f7ada9c353a2f28845fd61d6239365e4
victornwakpa <nwakpavictor@gmail.com>
2025-01-09 06:23:54 +0100

    fix footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,8 +7,8 @@ export default function Footer() {
     <div>
       <Spacer />
       <footer className="m-4 mx-auto mb-10 w-full rounded-lg bg-black/5 shadow backdrop-blur-lg xl:w-[1120px] dark:bg-white/5">
-        <div className="mx-auto w-full max-w-screen-xl p-4 md:flex md:justify-center">
-          <span className="text-sm text-blue-800/90 sm:text-center dark:text-blue-200/90">
+        <div className="mx-auto w-full max-w-screen-xl flex justify-center p-4">
+          <span className="text-sm text-blue-800/90 dark:text-blue-200/90">
             © {currentYear} {" "}
             Made with ❤️ by{" "}
             <Link href="/" className="hover:underline">
